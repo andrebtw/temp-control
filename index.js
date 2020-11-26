207356fa1d7575e16eac15dcfb273f1e27e59303
andrebtw <gwandrewgd@gmail.com>
2020-11-26 10:25:35 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const fs = require("fs")
 
-fs.readFile('temps.txt',(err, data) => {
-  if (err) throw err;
-  var temperature=data.toString();
-  var temperature=parseFloat(temperature)
-  console.log(temperature+temperature);
-  function min_temp() {
-    temp=temperature
-    console.log(Math.min(temp));
-    return Math.min(temp);
-  }
-  min_temp();
 
-})
+while (true) {
+
+  fs.readFile('temps.txt',(err, data) => {
+    if (err) throw err;
+    var temperature=data.toString();
+    var temperature=parseFloat(temperature)
+    console.log(temperature);
+    function min_temp() {
+      temp=temperature
+      console.log(Math.min(temp));
+      return Math.min(temp);
+    }
+    min_temp();
+
+  })
+
+}
