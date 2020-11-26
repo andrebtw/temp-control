6e1814d92106d1900a4e4627bbe6a26ae589a110
andrebtw <gwandrewgd@gmail.com>
2020-11-26 09:45:14 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require("fs")
 
 fs.readFile('temps.txt',(err, data) => {
   if (err) throw err;
-  console.log(data.toString());
+  var min_temperature=data.toString();
+  console.log(min_temperature);
 })
-
-var min_temperature=0;
