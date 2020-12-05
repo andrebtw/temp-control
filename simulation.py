821ce954f24aaac5d28f2347cd08140303243e63
andrebtw <gwandrewgd@gmail.com>
2020-12-05 23:49:11 +0100

    update

--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ import random
 
 for i in range(10):
     temp=random.randint(0,100)
-    f = open("demofile3.txt", "w")
+    f = open("temps.txt", "w")
     f.write(f"{temp}")
     f.close()
     f = open("demofile3.txt", "r")
