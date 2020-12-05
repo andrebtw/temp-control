ac181721015b91d524f00c7292e99a835135d7c9
andrebtw <gwandrewgd@gmail.com>
2020-12-06 00:00:48 +0100

    update

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ var fs = require('fs');
 
 
 
-
-// sleep time expects milliseconds
-function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-
-
-
 while (true) {
-  sleep(500).then(() => {
-    try {
-        var temps = fs.readFileSync('temps.txt', 'utf8');
-        console.log(temps);
-    } catch(e) {
-        console.log('Error:', e.stack);
-    }
-  });
+  try {
+      var temps = fs.readFileSync('temps.txt', 'utf8');
+      console.log(temps);
+  } catch(e) {
+      console.log('Error:', e.stack);
+  }
+
 }
