336ba80e15766e01517d4b85938faff2b5af4f4e
andrebtw <gwandrewgd@gmail.com>
2020-12-05 23:55:16 +0100

    update

--- simulation.py
+++ simulation.py
@@ -7,6 +7,6 @@ for i in range(10):
     f = open("temps.txt", "w")
     f.write(f"{temp}")
     f.close()
-    f = open("demofile3.txt", "r")
+    f = open("temps.txt", "r")
     print(f.read())
     time.sleep(1000)
