ed2d34ce48a50bacc58c6581f28da3d188341c07
andrebtw <gwandrewgd@gmail.com>
2020-12-05 23:53:34 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var fs = require('fs');
 
-try {
-    var temps = fs.readFileSync('temps.txt', 'utf8');
-    console.log(temps);
-} catch(e) {
-    console.log('Error:', e.stack);
+
+
+while (true) {
+  try {
+      var temps = fs.readFileSync('temps.txt', 'utf8');
+      console.log(temps);
+  } catch(e) {
+      console.log('Error:', e.stack);
+  }
+
 }
