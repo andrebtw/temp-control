38324b6c1ba72d429491b6d26af9f60dd37154c2
andrebtw <gwandrewgd@gmail.com>
2020-12-16 18:16:43 +0100

    update

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const fs = require('fs')
 
 function desired_temp() {
   var temp = document.getElementById("temp").value;
-  fs.writeFile('temps.txt', temp, (err) => { 
+  fs.writeFile('temps.txt', temp, (err) => {
 
     // In case of a error throw err.
     if (err) throw err;
 })
 
+}
+
+
+function display_temp() {
+  try {
+      var temps = fs.readFileSync('temps.txt', 'utf8');
+      var temp_display = document.getElementById("temps").innerHTML = "TEMPERATURE VOULUE :" + temps + "°C";
+  } catch(e) {
+      console.log('Error:', e.stack);
+  }
 }
