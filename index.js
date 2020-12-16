c3350fa6cd0946e29d1885d94be841a0e3721978
andrebtw <gwandrewgd@gmail.com>
2020-12-16 17:03:13 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var fs = require('fs');
+const fs = require('fs')
 
 
 function desired_temp() {
   var temp = document.getElementById("temp").value;
-  
+  fs.writeFile('temps.txt', temp, (err) => { 
+
+    // In case of a error throw err.
+    if (err) throw err;
+})
+
 }
