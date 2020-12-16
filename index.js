0380727bda945ba94613f1c9436a709c5cd5589c
andrebtw <gwandrewgd@gmail.com>
2020-12-16 18:45:01 +0100

    update

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const fs = require('fs')
 
 
+function display_temp() {
+    var temps = fs.readFileSync('temps.txt', 'utf8');
+    console.log(temps)
+    document.getElementById("temps").innerHTML = "TEMPERATURE VOULUE : " + temps + "°C";
+}
+
+display_temp();
+
 function desired_temp() {
   var temp = document.getElementById("temp").value;
   fs.writeFile('temps.txt', temp, (err) => {
-
+    display_temp();
     // In case of a error throw err.
     if (err) throw err;
 })
 
 }
-
-
-function display_temp() {
-  try {
-      var temps = fs.readFileSync('temps.txt', 'utf8');
-      console.log(temps)
-      document.getElementById("temps").innerHTML = "TEMPERATURE VOULUE : " + temps + "°C";
-  } catch(e) {
-      console.log('Error:', e.stack);
-  }
-}
-
-
-display_temp();
